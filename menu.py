3900c5378ca050506fa3ed2a7e91399931ba11b9
Konstantin Kolesov <P-A-T-R-I-O-T-kolesov@yandex.ru>
2022-06-10 21:35:49 +0300

    Создание функции предупреждение об ошибках

--- menu.py
+++ menu.py
@@ -7,11 +7,57 @@ def menu_selection(): #Выделение пунктов меню
 def cleaning(): # Очистка терминала
     print('\n' * 40)
 
-def invalid_menu_item(): # Рамка: Неверного пункт меню
-    print('|-|'* 13)
-    print('|-|', ' ' * 31, '|-|')
-    print('|-|', ' '*5,'Неверный пункт меню', ' '*5, '|-|')
-    print('|-|', ' ' * 31, '|-|')
+def invalid_menu_item(error = 'Неверный пункт меню'): # Рамка: Неверного пункт меню
+    indent = ' '
+    indent_2 = ' '
+    if len(error) >= 37: # Работа с длинными предупреждениями
+        error_1 = error[:len(error) // 2] # Делим на 2с равные части
+        error_2 = error[len(error) //2:]
+        
+        for char1 in error_2[:5]:  # второую часть, начало строки (до максимального размера страки которая может войти в строку) проверяем на знаки припенания
+            if char1 in [' ','.', ',', ':', ';', '!', '?']:
+                search1 = error_2.find(char1) # Ищем данный символ в строке
+                error_1 = error_1 + error_2[:search1:] #Слияние пер
+                error_2 = error_2[search1:] # вычитание не нужных символов
+        
+    border = '|-|' 
+    print(border * 13)    # Печатаем верхнюю рамки
+    len(border +indent * 33 + border)
+    
+    if len(error) >= 37: # В случаи когда главный параметр больше максимального значения, выравниваем по центру рамки
+        if len(border + indent + error_1 + indent_2 + border) < 39:
+            while len(border + indent + error_1 + indent_2 + border) <= 38:
+                indent += ' '
+                indent_2 += ' '
+            if len(border + indent + error_1 + indent_2 + border) > 39:
+                indent = indent[1:]
+            print(border + indent + error_1 + indent_2 + border)
+            indent = ' '
+            indent_2 = ' '
+        #else:
+        #    print(border, indent, error_1, indent_2, border)
+        if len(border + indent + error_2 + indent_2 + border) < 39:
+            while len(border + indent + error_2 + indent_2 + border) <= 38:
+                indent += ' '
+                indent_2 += ' '
+            if len(border + indent + error_2 + indent_2 + border) > 39:
+                indent = indent[1:]
+            print(border + indent + error_2 + indent_2 + border)
+    
+    elif len(error) < 37:  # В случаи когда главный параметр маленькое значение, выравниваем по центру рамки
+        indent = ' '
+        indent_2 = ' '
+        while len(border + indent + error + indent_2 + border) <= 38:
+            indent += ' '
+            indent_2 += ' '
+        if len(border + indent + error + indent_2 + border) > 39:
+                indent = indent[1:]
+        print(border + indent + error + indent_2 + border)
+    indent = ' '
+    print(border + indent * 33 + border)
+    print(border * 13)
+    #print('|-|', indent * 31, '|-|')
+    print('\n'* 2)
     print('|-|'* 13, '\n'* 2)
 
 while True: # Основное меню
@@ -68,3 +114,4 @@ while True: # Основное меню
         cleaning
     else:
         invalid_menu_item()
+        print(len('|-|'* 13))
